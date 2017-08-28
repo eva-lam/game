b4160b50cee4f45eaf1aaedb2a722371ea0e8580
Martin Chin <martinresx@gmail.com>
2017-08-28 13:57:08 +0800

    Revert "Revert "Martin add the Assests file, GameStart.js, index.html, HealthBar-StandAlone.js and Game.js""
    This reverts commit 925912465b196c610a163001a07dd8b75f8abbea.

---
+++ Game.js
@@ -0,0 +1,26 @@
+let game;
+
+
+window.onload = function() {
+    game = new Phaser.Game(640,960);
+    game.state.add("Boot", Boot);
+    game.state.add("GameTitleScreen", GameTitleScreen);
+    game.state.add("GameStart", GameStart);
+    game.state.add("GamePreload", GamePreload);
+	game.state.start("Boot");
+}
+class Boot {
+    preload(){
+        game.load.image('Wall', "Assets/Wall.png")
+    }
+	create() {
+        game.stage.backgroundColor = "#999999";
+		game.scale.pageAlignHorizontally = true;
+		game.scale.pageAlignVertically = true;
+		game.scale.scaleMode = Phaser.ScaleManager.SHOW_ALL;
+		game.state.start("GamePreload");
+	}
+}
+
+
+
